a275515f28bf1eb5cb4f73e90c7ca87a4ed41fb6
Shreshth Singhal <shreshth91@gmail.com>
2012-03-24 01:15:44 -0400

    forgot to add class for constants

---
+++ android/src/com/cos598b/Consts.java
@@ -0,0 +1,7 @@
+package com.cos598b;
+
+public class Consts {
+	public static final int time_granularity = 60; // time granularity for location updates (in seconds)
+	public static final int time_diff = 5;         // time difference between two GPS readings for direction calculation
+	public static final int dist_granularity = 2;  // distance granularity for location updates (in metres)
+}
