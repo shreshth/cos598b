88b0cfc417e673819f41772ddf3c7b6e04c61e33
Shreshth Singhal <shreshth91@gmail.com>
2012-03-26 01:02:52 -0400

    sending data to backend - more stuff - just need to figure out how to package it for sending

--- android/src/com/cos598b/Consts.java
+++ android/src/com/cos598b/Consts.java
@@ -7,5 +7,6 @@ public class Consts {
 	public static final int time_diff = 5;         // time difference between two GPS readings for direction calculation
 	public static final int dist_granularity = 2;  // distance granularity for location updates (in metres)
 	public static final int num_loc_steps = 10;    // how many steps of location data to store
+	public static final int buf_size = 512;        // how many data points to store before needing to send to back-end
 	public static final String device_id = Settings.Secure.ANDROID_ID; // unique ID for device
 }
