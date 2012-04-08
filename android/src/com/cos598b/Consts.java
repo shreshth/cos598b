5faaa2b3104f5dfe7c6c92137849a493c6b36575
hamzaaftab <hamzaaftabmalik@gmail.com>
2012-04-07 23:38:44 -0400

    added alarms. checking for wifi. use bearing instead of 5 second gps

--- android/src/com/cos598b/Consts.java
+++ android/src/com/cos598b/Consts.java
@@ -3,10 +3,18 @@ package com.cos598b;
 import android.provider.Settings;
 
 public class Consts {
-	public static final int time_granularity = 0; //60; // time granularity for location updates (in seconds)
-	public static final int time_diff = 5;         // time difference between two GPS readings for direction calculation
-	public static final int dist_granularity = 0; //2;  // distance granularity for location updates (in metres)
-	public static final int num_loc_steps = 10;    // how many steps of location data to store
-	public static final int buf_size = 512;        // how many data points to store before needing to send to back-end
-	public static final String device_id = Settings.Secure.ANDROID_ID; // unique ID for device
+    // time granularity for location updates (in seconds)
+    public static final int time_granularity = 60;
+
+    // maximum wait for a gps location / wifi scan to return (in seconds)
+    public static final int max_wait = 10;
+
+    // how many steps of location data to store
+    public static final int num_loc_steps = 10;
+
+    // supported wireless SSID's
+    public static final String[] ssids = {"puwireless", "csvapornet"};
+
+    // unique ID for device
+    public static final String device_id = Settings.Secure.ANDROID_ID;
 }
--- android/src/com/cos598b/Home.java
+++ android/src/com/cos598b/Home.java
@@ -4,8 +4,8 @@
  * 2. If user doesn't turn GPS on, then quit?
  * 3. If GPS not returning a location (e.g. indoors), then? In this case, it just ignores all this data.
  * 4. So far, only checking if Wifi is *connected*. Should check if any open Wifi is available?
- * 5. If there is movement in 60 seconds, but no movement in 5-second span, we can't detect direction. Currently, it uses the lat/long 
- *    from the previous 60 second span (less accurate since more motion in 60 seconds than 5). Is this okay, 
+ * 5. If there is movement in 60 seconds, but no movement in 5-second span, we can't detect direction. Currently, it uses the lat/long
+ *    from the previous 60 second span (less accurate since more motion in 60 seconds than 5). Is this okay,
  *    or should we just discard that data?
  * 6. What if we go out of range of GPS and then get WiFi (e.g. enter a building and then get WiFi). In this
  * 	  case, we wouldn't get any location updates, and will not be capture the fact that WiFi was found.
@@ -13,156 +13,66 @@
 
 package com.cos598b;
 
-import java.lang.reflect.Method;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.telephony.TelephonyManager;
-import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class Home extends Activity {
-	
-	
-	private boolean isAvailableWiFi() {
-		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo[] ni = cm.getAllNetworkInfo();
-		for (NetworkInfo n : ni) {
-			if (n.getTypeName().equalsIgnoreCase("wifi")) {
-				return true;
-			}
-		}
-		return false;
-	}
 
-	/* is WiFi connected */
-	private boolean isConnectedWiFi() {
-		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo[] ni = cm.getAllNetworkInfo();
-		for (NetworkInfo n : ni) {
-			if ((n.getTypeName().equalsIgnoreCase("wifi")) && (n.isConnected())) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	/* is mobile data connected? */
-	private boolean isConnectedMobile() {
-		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo[] ni = cm.getAllNetworkInfo();
-		for (NetworkInfo n : ni) {
-			if ((n.getTypeName().equalsIgnoreCase("mobile")) && (n.isConnected())) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	/* stop WiFi, if enabled */
-	private void disableWiFi() {
-		WifiManager wm = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-		if (wm.isWifiEnabled()) { wm.setWifiEnabled(false); }
-	}
-	
-	/* start WiFi, if disabled */
-	private void enableWiFi() {
-		WifiManager wm = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-		if (!wm.isWifiEnabled()) { wm.setWifiEnabled(true); }
-	}
-	
-	private void stop3G() {
-		boolean isEnabled;
-		Method dataConnSwitchmethod;
-	    Class telephonyManagerClass;
-	    Object ITelephonyStub;
-	    Class ITelephonyClass;
-	    
-		TelephonyManager telephonyManager = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+    /* is WiFi connected */
+    private boolean isConnectedWiFi() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] ni = cm.getAllNetworkInfo();
+        for (NetworkInfo n : ni) {
+            if (n.getTypeName().equalsIgnoreCase("wifi") && n.isConnected()) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-		if(telephonyManager.getDataState() == TelephonyManager.DATA_CONNECTED){
-			isEnabled = true;
-		} else {
-			isEnabled = false;  
-		}   
-		
-		try {
-			telephonyManagerClass = Class.forName(telephonyManager.getClass().getName());
-			Method getITelephonyMethod = telephonyManagerClass.getDeclaredMethod("getITelephony");
-			getITelephonyMethod.setAccessible(true);
-			ITelephonyStub = getITelephonyMethod.invoke(telephonyManager);
-			ITelephonyClass = Class.forName(ITelephonyStub.getClass().getName());
+    /* is mobile data connected? */
+    private boolean isConnectedMobile() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] ni = cm.getAllNetworkInfo();
+        for (NetworkInfo n : ni) {
+            if (n.getTypeName().equalsIgnoreCase("mobile") && n.isConnected()) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-			if (isEnabled) {
-				dataConnSwitchmethod = ITelephonyClass.getDeclaredMethod("disableDataConnectivity");
-			} else {
-				dataConnSwitchmethod = ITelephonyClass.getDeclaredMethod("enableDataConnectivity");   
-			}
-			dataConnSwitchmethod.setAccessible(true);
-			dataConnSwitchmethod.invoke(ITelephonyStub);
-		} catch (Exception e) {} 
-		/*
-		final ConnectivityManager conman = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
-		try {
-			final Class conmanClass = Class.forName(conman.getClass().getName());
-			final Field iConnectivityManagerField = conmanClass.getDeclaredField("mService");
-			iConnectivityManagerField.setAccessible(true);
-			final Object iConnectivityManager = iConnectivityManagerField.get(conman);
-			final Class iConnectivityManagerClass = Class.forName(iConnectivityManager.getClass().getName());
-			final Method setMobileDataEnabledMethod = iConnectivityManagerClass.getDeclaredMethod("setMobileDataEnabled", Boolean.TYPE);
-			setMobileDataEnabledMethod.setAccessible(true);
+    /* stop WiFi, if enabled */
+    private void disableWiFi() {
+        WifiManager wm = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        if (wm.isWifiEnabled()) { wm.setWifiEnabled(false); }
+    }
 
-			setMobileDataEnabledMethod.invoke(iConnectivityManager, true);
-		} catch (Exception e) {}
-		*/
-	}
+    /* start WiFi, if disabled */
+    private void enableWiFi() {
+        WifiManager wm = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        if (!wm.isWifiEnabled()) { wm.setWifiEnabled(true); }
+    }
 
-	/* Get best known location */
-	private Location getLocation() {
-		LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		
-		//lm.addGpsStatusListener(listener);
-		// if GPS is disabled, ask user to turn it on // XXX : 1
-		if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-			Intent gpsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-			startActivity(gpsIntent);
-		}
-		// regardless of whether turned on or not, use best location tracker // XXX : 2		
-		Criteria criteria = new Criteria();
-		criteria.setAccuracy(Criteria.ACCURACY_FINE);
-		String best_provider = lm.getBestProvider(criteria, true);
-		// Log.d("B", best_provider)); // DEBUG
-		Location location;
-		while ((location = lm.getLastKnownLocation(best_provider)) == null) {
-			Log.d("C", "NULL");
-		}
-		return location;
-		//return lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-	}
-   
-	
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		TextView tv = new TextView(this);
+    private void stop3G() {
+        // impossibru
+    }
 
-		/*
-		Location location = getLocation();
-		Log.d("A", "Lat: " + location.getLatitude() + " Long: " + location.getLongitude());
-		*/
-		startService(new Intent(this, LocationTracker.class));
-		tv.setText("Connected to wifbi");
-		setContentView(tv);		
-	}	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+
+        Intent intent = new Intent(this, MarkovService.class);
+        intent.putExtra("METHOD", "start");
+        startService(new Intent(this, MarkovService.class));
+        setContentView(tv);
+    }
 }
