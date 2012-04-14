04c2187d4d737a6b8b8fab8871b6520dc53c19c7
hamzaaftab <hamzaaftabmalik@gmail.com>
2012-04-14 15:00:41 -0400

    store accuracy and speed: backend

--- appengine/add_data.py
+++ appengine/add_data.py
@@ -13,12 +13,14 @@ class AddData(webapp.RequestHandler):
         lat = str.split(str(self.request.get('lat')),',')
         lng = str.split(str(self.request.get('lng')),',')
         bearing = str.split(str(self.request.get('bearing')),',')
+        speed = str.split(str(self.request.get('speed')),',')
+        accuracy = str.split(str(self.request.get('accuracy')),',')
         time = str.split(str(self.request.get('time')),',')
         timestamp = str.split(str(self.request.get('timestamp')),',')
         # Read user id
         user_id = self.request.get('user_id')
         # All arrays must have the same size
-        if ((len(lat) != len (lng)) or (len(lat) != len (bearing)) or (len(lat) != len (time)) or (len(lat) != len (timestamp))):
+        if ((len(lat) != len (lng)) or (len(lat) != len (bearing)) or (len(lat) != len (time)) or (len(lat) != len (timestamp)) or (len(lat) != len (speed)) or (len(lat) != len (accuracy))):
             self.error(400)
         # Add each data point to the database
         for i in range(len(lat)):
@@ -26,7 +28,7 @@ class AddData(webapp.RequestHandler):
             # time >= 0 means wifi was obtained eventually
             wifi = (int(time[i]) >= 0)
             # Store data point
-            point = entities.Point(location=location, bearing=float(bearing[i]), wifi=wifi, time=int(time[i]), timestamp=int(timestamp[i]), user_id = user_id)
+            point = entities.Point(location=location, bearing=float(bearing[i]), speed=float(speed[i]), accuracy=float(accuracy[i]), wifi=wifi, time=int(time[i]), timestamp=int(timestamp[i]), user_id = user_id)
             point.put()
 
 application = webapp.WSGIApplication([('/.*', AddData)], debug=True)
--- appengine/entities.py
+++ appengine/entities.py
@@ -5,7 +5,9 @@ Data type representing a single data point
 """
 class Point(db.Model):
     location = db.GeoPtProperty(required=True)                          # GPS locations
-    bearing = db.FloatProperty(required=True)                             # Direction of movement in radians (+pi to -pi)
+    bearing = db.FloatProperty(required=True)                           # Direction of movement in radians (+pi to -pi)
+    speed = db.FloatProperty(required=True)                             # Speed of movement
+    accuracy = db.FloatProperty(required=True)                          # Accuracy of GPS movement
     wifi = db.BooleanProperty(required=True)                            # Whether wifi was obtained eventually
     time = db.IntegerProperty(default=None, required=False)             # if wifi == true
     timestamp = db.IntegerProperty(required=True)                       # the timestamp (time since 1970) in ms
